b5b7211d3fbe9af9fa196797d9b5b0510c0d4766
Nahuel <nahu.abello@gmail.com>
2025-02-11 18:42:13 -0300

    se especifico la entrada de textos, agrego funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.let
+let amigos = []
+function agregarAmigo(){
+    let entradaDeTextos = document.getElementById("amigo")
+    let nombreDeAmigo = entradaDeTextos.value
+    if (amigos.includes(nombreDeAmigo)){
+        console.info(`${nombreDeAmigo} ya fue añadido`)
+        return;
+    }
+    console.log(nombreDeAmigo)
+    amigos.push(nombreDeAmigo)
+}  
+
+
+function sortearAmigo(){}
